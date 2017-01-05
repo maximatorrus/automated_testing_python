66aeda2990cc3a5e2c08455b1f5ee423b2253bdd
Maxim Dymchenko <maximatorrus@mail.ru>
2017-01-05 15:00:21 +0300

    fixture done

--- firs_test_add_group.py
+++ firs_test_add_group.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class firs_test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.app.login(user_name="admin", password="secret")
-        self.app.create_group(Group(name="group2", header="logo2", footer="comment2"))
-        self.app.logout()
 
-    def test_empty_group(self):
-        self.app.login(user_name="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_add_group(app):
+    app.login(user_name="admin", password="secret")
+    app.create_group(Group(name="group2", header="logo2", footer="comment2"))
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == '__main__':
-    unittest.main()
+def test_empty_group(app):
+    app.login(user_name="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
