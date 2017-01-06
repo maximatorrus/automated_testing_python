a38ebe25f6520e888e17b55ce6c8f8c95c542c1b
Maxim Dymchenko <maximatorrus@mail.ru>
2017-01-07 01:08:45 +0300

    сравнение после добавления группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,11 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group2", header="logo2", footer="comment2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
-def test_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+#def test_empty_group(app):
+#    app.group.create(Group(name="", header="", footer=""))
--- test/test_edit_user.py
+++ test/test_edit_user.py
@@ -12,11 +12,7 @@ def test_edit_first_user(app):
                              bmonth="//div[@id='content']/form/select[2]//option[7]",
                              aday="//div[@id='content']/form/select[3]//option[15]",
                              amonth="//div[@id='content']/form/select[4]//option[6]"))
-    app.user.edit_first(User(firstname="Second", middlename="Middle2", lastname="Last2", nickname="Nickname2",
-                         title="Title2", company="Company2", address="Address", telephone="telephone", mobile="mobile2",
-                         work="work2", fax="fax2", email_="email12", email2="email22", email3="email32",
-                         homepage="homepage", byear="1991", ayear="2000",
-                         bday="//div[@id='content']/form/select[1]//option[14]",
+    app.user.edit_first(User(firstname="Second",
                          bmonth="//div[@id='content']/form/select[2]//option[7]",
                          aday="//div[@id='content']/form/select[3]//option[15]",
                          amonth="//div[@id='content']/form/select[4]//option[6]"))
