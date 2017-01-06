974d918b9fec95892ed029bc4ebf1a8d149246ac
Maxim Dymchenko <maximatorrus@mail.ru>
2017-01-06 18:21:03 +0300

    test state

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="group2", header="logo2", footer="comment2"))
+#    app.session.logout()
 
 
 def test_empty_group(app):
