fb1cd139a4a4f4687a0360127a57a167c44300dd
Maxim Dymchenko <maximatorrus@mail.ru>
2017-01-09 01:53:04 +0300

    задание 15

--- model/user.py
+++ model/user.py
@@ -34,7 +34,7 @@ class User:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname\
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.user import User
+import pytest
+import random
+import string
 
 
-def test_add_user(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + " ".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    User(firstname=firstname, lastname=lastname, address=address, telephone=telephone, email_=email_)
+    for firstname in ["", random_string("firstname", 10)]
+    for lastname in ["", random_string("lastname", 10)]
+    for address in ["", random_string("address", 10)]
+    for telephone in ["", random_string("telephone", 10)]
+    for email_ in ["", random_string("email_", 10)]
+    ]
+
+
+@pytest.mark.parametrize("user", testdata, ids=[repr(x) for x in testdata])
+def test_add_user(app, user):
     old_users = app.user.get_user_list()
-    user = User(firstname="First", middlename="Middle", lastname="Last", nickname="Nickname",
-                         title="Title", company="Company", address="Address", telephone="telephone", mobile="mobile",
-                         work="work", fax="fax", email_="email1", email2="email2", email3="email3",
-                         homepage="homepage", byear="1991", ayear="2000",
-                         bday="//div[@id='content']/form/select[1]//option[14]",
-                         bmonth="//div[@id='content']/form/select[2]//option[7]",
-                         aday="//div[@id='content']/form/select[3]//option[15]",
-                         amonth="//div[@id='content']/form/select[4]//option[6]")
-    app.user.create(user)
-    new_users = app.user.get_user_list()
-    assert len(old_users) + 1 == len(new_users)
-    old_users.append(user)
-    assert sorted(old_users, key=User.id_or_max) == sorted(new_users, key=User.id_or_max)
-
-
-def test_add__empty_user(app):
-    old_users = app.user.get_user_list()
-    user = User(firstname="", middlename="", lastname="", nickname="",
-                         title="", company="", address="", telephone="",
-                         mobile="",
-                         work="", fax="", email_="", email2="", email3="",
-                         homepage="", byear="", ayear="",
-                         bday="//div[@id='content']/form/select[1]//option[1]",
-                         bmonth="//div[@id='content']/form/select[2]//option[1]",
-                         aday="//div[@id='content']/form/select[3]//option[1]",
-                         amonth="//div[@id='content']/form/select[4]//option[1]")
     app.user.create(user)
     new_users = app.user.get_user_list()
     assert len(old_users) + 1 == len(new_users)
